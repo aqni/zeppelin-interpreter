32cf8aaffc9cc605fe9a543f0d67228510240d24
liuxtq <liu_xtq@163.com>
2024-12-10 17:38:30 +0800

    1.修复bug，zeppelin notebook刷新后页面元素重排导致css选择器失效。

--- v8/src/main/resources/static/highcharts/hideTableBtns.js
+++ v8/src/main/resources/static/highcharts/hideTableBtns.js
@@ -1,3 +1,5 @@
 <script>
-    document.querySelectorAll('[id=\"PARAGRAPH_ID_container\"] > div:nth-child(1) > .tableDisplay.ng-scope')[TABLE_NUMBER].querySelector("div:nth-child(1) > div:nth-child(1)").style.display='none';
+    setTimeout(function(){
+        document.querySelectorAll('[id=\"PARAGRAPH_ID_container\"] > div:nth-child(1) > .tableDisplay.ng-scope')[TABLE_NUMBER].querySelector("div:nth-child(1) > div:nth-child(1)").style.display='none';
+    }, 10);
 </script>
